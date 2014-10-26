42e442781f39e317586433cf4898b8eab8e251a7
Peter Gow <peter.gow@gmail.com>
2014-10-26 22:15:04 +1100

    Initial Commit

---
+++ Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Peter Gow on 26/10/2014.
+//  Copyright (c) 2014 Peter Gow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Peter Gow on 26/10/2014.
+//  Copyright (c) 2014 Peter Gow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
