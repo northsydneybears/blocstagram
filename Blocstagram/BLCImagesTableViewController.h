c4b617122f8d6cb3eb28b2979e7549b0935b8900
Peter Gow <peter.gow@gmail.com>
2014-10-30 02:10:20 +1100

    Table View controller implemented

---
+++ Blocstagram/BLCImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Peter Gow on 26/10/2014.
+//  Copyright (c) 2014 Peter Gow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
