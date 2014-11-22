6d67525c570f98c6662d1b3d228978c85215f7ab
Peter Gow <peter.gow@gmail.com>
2014-11-22 21:07:22 +1100

    Podfile Mach 0 error

--- BLCMedia.h
+++ BLCMedia.h
@@ -12,7 +12,7 @@
 @class BLCUser;
 @class BLCComment;
 
-@interface BLCMedia : NSObject
+@interface BLCMedia : NSObject <NSCoding>
 
 @property (nonatomic, strong) NSString *idNumber;
 @property (nonatomic, strong) BLCUser *user;
